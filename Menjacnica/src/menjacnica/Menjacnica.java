a4c4563b4c4a53551fe43116e0edf48cdc1c3944
Dragutin Todorovic <Dragutin Todorovic@DESKTOP-3K036KM>
2018-03-17 16:46:37 +0100

    Dodate su hashcode, equals i toString metode svim klasama
    v1.1

--- Menjacnica/src/menjacnica/Menjacnica.java
+++ Menjacnica/src/menjacnica/Menjacnica.java
@@ -5,5 +5,41 @@ import java.util.LinkedList;
 public class Menjacnica {
 	private String naziv;
 	private LinkedList<Valuta> valute;
-	
+
+	@Override
+	public String toString() {
+		return "Menjacnica [naziv=" + naziv + ", valute=" + valute + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((naziv == null) ? 0 : naziv.hashCode());
+		result = prime * result + ((valute == null) ? 0 : valute.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Menjacnica other = (Menjacnica) obj;
+		if (naziv == null) {
+			if (other.naziv != null)
+				return false;
+		} else if (!naziv.equals(other.naziv))
+			return false;
+		if (valute == null) {
+			if (other.valute != null)
+				return false;
+		} else if (!valute.equals(other.valute))
+			return false;
+		return true;
+	}
+
 }
